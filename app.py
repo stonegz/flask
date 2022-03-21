1514317a0ad3df2061b725f16e4af1819a6cb407
stone <stonegz@icloud.com>
2022-03-21 23:05:50 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# save this as app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
